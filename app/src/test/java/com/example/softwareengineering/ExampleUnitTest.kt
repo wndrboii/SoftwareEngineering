9ea5cada9050051f55138a4913163fddaa87f308
ELOKT <ul0266104@edu.uni.lodz.pl>
2024-04-18 01:03:43 +0200

    CookIt_vers_1.3
    Signed-off-by: ELOKT <ul0266104@edu.uni.lodz.pl>

--- app/src/test/java/com/example/softwareengineering/ExampleUnitTest.kt
+++ app/src/test/java/com/example/softwareengineering/ExampleUnitTest.kt
@@ -15,3 +15,10 @@ class ExampleUnitTest {
         assertEquals(4, 2 + 2)
     }
 }
+
+class ExampleUnitTest 2{
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(6, 3 + 3)///
+    }
+}
