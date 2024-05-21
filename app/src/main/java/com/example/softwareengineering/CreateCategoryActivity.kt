d385035e2728c929772ede4d7edce262e33bb825
ELOKT <ul0266104@edu.uni.lodz.pl>
2024-05-21 03:33:54 +0200

    CookIt_vers_2_2 full

---
+++ app/src/main/java/com/example/softwareengineering/CreateCategoryActivity.kt
@@ -0,0 +1,90 @@
+package com.example.softwareengineering
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import com.example.softwareengineering.model.DishCategory
+import com.example.softwareengineering.model.ProductCategory
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class CreateCategoryActivity : AppCompatActivity() {
+
+    private lateinit var logout: ImageButton
+    private lateinit var home: ImageButton
+    private lateinit var categories: ImageButton
+    private lateinit var categoryName: EditText
+    private lateinit var addButton: ImageButton
+    private lateinit var kategorieArr: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_category)
+
+        logout = findViewById(R.id.logout_button)
+        home = findViewById(R.id.home_button)
+        categories = findViewById(R.id.categories_btn)
+
+        addButton = findViewById(R.id.submit_btn)
+
+        //Menu navigation
+        home.setOnClickListener(View.OnClickListener {
+            var intent: Intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        })
+
+        categories.setOnClickListener(View.OnClickListener {
+            var intent: Intent = Intent(applicationContext, CategoriesActivity::class.java)
+            startActivity(intent)
+            finish()
+        })
+
+        logout.setOnClickListener(View.OnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            var intent: Intent = Intent(applicationContext, login::class.java)
+            startActivity(intent)
+            finish()
+        })
+
+        kategorieArr = findViewById(R.id.kategorie_arr_btn)
+        kategorieArr.setOnClickListener(View.OnClickListener {
+            var intent: Intent = Intent(applicationContext, DishCatActivity::class.java)
+            startActivity(intent)
+            finish()
+        })
+
+        addButton.setOnClickListener {
+            categoryName = findViewById<EditText>(R.id.name_edit_text)
+            val name = categoryName.text.toString()
+
+            if (name.isNotEmpty()) {
+                val database = Firebase.database.reference
+
+                val cat = DishCategory(
+                    id = database.child("categories").push().key,
+                    name = name,
+                )
+
+                if (cat.id != null) {
+                    database.child("categories").child(cat.id!!).setValue(cat).addOnSuccessListener {
+                        Toast.makeText(this, "Nowa kategoria dodana pomyślnie", Toast.LENGTH_SHORT).show()
+                        categoryName.text.clear()
+                    }
+                        .addOnFailureListener {
+                            Toast.makeText(
+                                this,
+                                "Błąd podczas dodawania kategorii: ${it.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                }
+            } else {
+                Toast.makeText(this, "Please enter a name for the category", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/softwareengineering/NotificationUtils.kt
@@ -0,0 +1,86 @@
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.example.softwareengineering.R
+import com.example.softwareengineering.model.DailyNutrition
+import java.text.SimpleDateFormat
+import java.util.*
+
+class NotificationUtils {
+    private val CHANNEL_ID = "YourChannelId"
+
+    // Method to schedule the notification
+    @SuppressLint("UnspecifiedImmutableFlag")
+    fun scheduleNotification(context: Context, dailyNutrition: DailyNutrition) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+        val targetTime = dateFormat.parse(dailyNutrition.time)
+
+        val calendar = Calendar.getInstance()
+        if (targetTime != null) {
+            calendar.time = targetTime
+        }
+
+        val requestCode = 0
+        val notificationIntent = Intent(context, NotificationReceiver::class.java)
+        notificationIntent.putExtra("NOTIFICATION_MESSAGE", "Your notification message")
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            requestCode,
+            notificationIntent,
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+            } else {
+                PendingIntent.FLAG_UPDATE_CURRENT
+            }
+        )
+
+        alarmManager.setExact(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            pendingIntent
+        )
+    }
+
+    // Method to create and show the notification
+    fun showNotification(context: Context, message: String) {
+        val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentTitle("Notification Title")
+            .setContentText(message)
+            .setSmallIcon(R.drawable.ic_posilki)
+            .setAutoCancel(true)
+
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Create a notification channel for devices running Android 8.0 and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Your Channel Name",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val message = intent.getStringExtra("NOTIFICATION_MESSAGE")
+        // Show the notification
+        if (message != null) {
+            NotificationUtils().showNotification(context, message)
+        }
+    }
+}
